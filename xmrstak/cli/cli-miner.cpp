cc717fd6f55ac41614188bded907ad500ddb87dc
Snow Flake <snow-flake@mercury-wireless.com>
2017-12-14 21:34:07 -0500

    Fixed endif

--- xmrstak/cli/cli-miner.cpp
+++ xmrstak/cli/cli-miner.cpp
@@ -142,7 +142,6 @@ std::string get_multipool_entry(bool& final)
 	getline(std::cin, passwd);
 
 	bool tls = read_yes_no("- Does this pool port support TLS/SSL? Use no if unknown. (y/N)");
-#endif
 	bool nicehash = read_yes_no("- Do you want to use nicehash on this pool? (y/n)");
 
 	int64_t pool_weight;
