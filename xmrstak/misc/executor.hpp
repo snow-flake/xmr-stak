49d4964e9f9ac8961e976b0fc7843b0902b2915d
snow-flake <30215944+snow-flake@users.noreply.github.com>
2017-12-16 10:45:48 -0500

    Removed is_dev_time helper method (#5)

--- xmrstak/misc/executor.hpp
+++ xmrstak/misc/executor.hpp
@@ -5,7 +5,6 @@
 #include "xmrstak/backend/iBackend.hpp"
 #include "xmrstak/misc/environment.hpp"
 #include "xmrstak/net/msgstruct.hpp"
-#include "xmrstak/donate-level.hpp"
 
 #include <atomic>
 #include <array>
@@ -58,17 +57,6 @@ private:
 	// Dev donation time period in seconds. 100 minutes by default.
 	// We will divide up this period according to the config setting
 	constexpr static size_t iDevDonatePeriod = 100 * 60;
-
-	inline bool is_dev_time()
-	{
-		//Add 2 seconds to compensate for connect
-		constexpr size_t dev_portion = double(iDevDonatePeriod) * fDevDonationLevel + 2;
-
-		if(dev_portion < 12) //No point in bothering with less than 10s
-			return false;
-
-		return (get_timestamp() - dev_timestamp) % iDevDonatePeriod >= (iDevDonatePeriod - dev_portion);
-	};
 
 	std::list<timed_event> lTimedEvents;
 	std::mutex timed_event_mutex;
