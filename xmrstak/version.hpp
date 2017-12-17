4695e7d0a1032228fbabcbcf659ce8a47c7c7244
Snow Flake <snow-flake@mercury-wireless.com>
2017-12-16 21:03:26 -0500

    Added semi-colon

--- xmrstak/version.hpp
+++ xmrstak/version.hpp
@@ -5,7 +5,7 @@
 
 inline std::string get_version_str()
 {
-	return std::string("xmr-stak/2.1.0/31a078416042d5492540f4bdb1466a84b690430e/master/lin/cpu/monero/0")
+	return std::string("xmr-stak/2.1.0/31a078416042d5492540f4bdb1466a84b690430e/master/lin/cpu/monero/0");
 }
 
 inline std::string get_version_str_short()
