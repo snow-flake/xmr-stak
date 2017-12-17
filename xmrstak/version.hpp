d0a8804757520a3b622716ba3bdc4731fa1a7c05
snow-flake <30215944+snow-flake@users.noreply.github.com>
2017-12-16 21:17:34 -0500

    Hard code the version (#10)

--- xmrstak/version.hpp
+++ xmrstak/version.hpp
@@ -3,16 +3,12 @@
 #include <inttypes.h>
 #include <string>
 
-extern const char ver_long[];
-extern const char ver_short[];
-extern const char ver_html[];
-
 inline std::string get_version_str()
 {
-	return std::string(ver_long) + std::to_string(uint32_t(2.0 / 100.0)) ;
+	return std::string("xmr-stak/2.1.0/31a078416042d5492540f4bdb1466a84b690430e/master/lin/cpu/monero/0");
 }
 
 inline std::string get_version_str_short()
 {
-	return std::string(ver_short);
+	return std::string("xmr-stak 2.1.0 31a078416042d5492540f4bdb1466a84b690430e");
 }
