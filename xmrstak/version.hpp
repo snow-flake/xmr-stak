8c3e059afd54b35a7f8de55bc7d9f1d6a126a387
Snow Flake <snow-flake@mercury-wireless.com>
2017-12-20 19:45:01 -0500

    Making the pool config static

--- xmrstak/version.hpp
+++ xmrstak/version.hpp
@@ -3,6 +3,19 @@
 #include <inttypes.h>
 #include <string>
 
+#ifndef POOL_ADDRESS
+#define POOL_ADDRESS "xmr-us-east1.nanopool.org"
+#endif
+
+#ifndef POOL_PORT
+#define POOL_PORT "14444"
+#endif
+
+#ifndef POOL_TLS
+#define POOL_TLS false
+#endif
+
+
 inline std::string get_version_str()
 {
 	return std::string("xmr-stak/2.1.0/31a078416042d5492540f4bdb1466a84b690430e/master/lin/cpu/monero/0");
@@ -11,4 +24,8 @@ inline std::string get_version_str()
 inline std::string get_version_str_short()
 {
 	return std::string("xmr-stak 2.1.0 31a078416042d5492540f4bdb1466a84b690430e");
+}
+
+inline std::string get_pool_address() {
+	return std::string("")
 }
